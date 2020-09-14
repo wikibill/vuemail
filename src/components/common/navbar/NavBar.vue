2f5c53cd056b5e2a6a5a90890608ea2b15d3cd31
顶顶顶 <1689336897@qq.com>
2020-09-14 21:20:41 +0800

    轮播图完成

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -1,27 +1,30 @@
 <!--  -->
 <template>
-<div class='home'>
-  <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-  <h2>home</h2>
+<div class='navbar'>
+  <div class="left">
+    <slot name="left"></slot>
+  </div>
+  <div class="center">
+    <slot name="center"></slot>
+  </div>
+  <div class="right">
+    <slot name="right"></slot>
+  </div>
 </div>
 </template>
 
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
-import NavBar from '@/components/common/navbar/NavBar'
-import {getHomeMultidata} from '@/network/home'
+
 export default {
 //import引入的组件需要注入到对象中才能使用
-name:'home',
-components: {
-  NavBar
-},
+name:'navbar',
+components: {},
 data() {
 //这里存放数据
 return {
-  banners:[],
-  recommends:[]
+
 };
 },
 //监听属性 类似于data概念
@@ -34,11 +37,7 @@ methods: {
 },
 //生命周期 - 创建完成（可以访问当前this实例）
 created() {
-  getHomeMultidata().then(res=>{
-    console.log(res);
-    this.banners = res.data.banner.list
-    this.recommends = res.data.recommend.list
-  })
+
 },
 //生命周期 - 挂载完成（可以访问DOM元素）
 mounted() {
@@ -54,10 +53,18 @@ activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
 }
 </script>
 <style>
-.home-nav{
-  background-color: pink;
-  color: white;
-  font-weight: 600;
-  font-size: 18px;
+.navbar{
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  /* background-color: pink; */
+  text-align: center;
+  box-shadow: 0 1px 1px rgba(100,100,100,.2);
+}
+.left,.right{
+  width: 60px;
+}
+.center{
+  flex: 1;
 }
 </style>
---
+++ src/network/request.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export function request(config) {
+  //1.创建axios的实例
+  const instance = axios.create({
+    baseURL: "http://123.207.32.32:8000",
+    timeout: 5000
+  });
+  //2.axios的拦截
+  instance.interceptors.request.use(
+    config => {
+      console.log(config);
+      //一定要记得return出去
+      return config;
+    },
+    err => {
+      // console.log(err);
+    }
+  );
+
+  instance.interceptors.response.use(
+    res => {
+      console.log(res);
+      //返回处理结果
+      return res.data;
+    },
+    err => {
+      console.log(err);
+    }
+  );
+
+  //3.发送真正的网络请求
+  return instance(config);
+}
