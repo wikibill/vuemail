02ba0d6c2a0cb2cb745b7e9077de1f99ea490220
顶顶顶 <1689336897@qq.com>
2020-09-15 20:52:38 +0800

    swiper and recommends

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,60 +1,66 @@
 <!--  -->
 <template>
-<div class='home'>
-  <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-  <h2>home</h2>
-</div>
+  <div class="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend-view :recommends="recommends"></home-recommend-view>
+  </div>
 </template>
 
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
-import NavBar from '@/components/common/navbar/NavBar'
-import {getHomeMultidata} from '@/network/home'
+import NavBar from "@/components/common/navbar/NavBar";
+import { getHomeMultidata } from "@/network/home";
+import HomeSwiper from "./childComps/HomeSwiper";
+import HomeRecommendView from "./childComps/HomeRecommendView";
+// import Swiper from '@/components/common/swiper/Swiper'
+// import SwiperItem from '@/components/common/swiper/SwiperItem'
+// import { Swiper, SwiperItem } from "@/components/common/swiper";
 export default {
-//import引入的组件需要注入到对象中才能使用
-name:'home',
-components: {
-  NavBar
-},
-data() {
-//这里存放数据
-return {
-  banners:[],
-  recommends:[]
+  //import引入的组件需要注入到对象中才能使用
+  name: "home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    HomeRecommendView,
+  },
+  data() {
+    //这里存放数据
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+    getHomeMultidata().then((res) => {
+      // console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
 };
-},
-//监听属性 类似于data概念
-computed: {},
-//监控data中的数据变化
-watch: {},
-//方法集合
-methods: {
-
-},
-//生命周期 - 创建完成（可以访问当前this实例）
-created() {
-  getHomeMultidata().then(res=>{
-    console.log(res);
-    this.banners = res.data.banner.list
-    this.recommends = res.data.recommend.list
-  })
-},
-//生命周期 - 挂载完成（可以访问DOM元素）
-mounted() {
-
-},
-beforeCreate() {}, //生命周期 - 创建之前
-beforeMount() {}, //生命周期 - 挂载之前
-beforeUpdate() {}, //生命周期 - 更新之前
-updated() {}, //生命周期 - 更新之后
-beforeDestroy() {}, //生命周期 - 销毁之前
-destroyed() {}, //生命周期 - 销毁完成
-activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
-}
 </script>
 <style>
-.home-nav{
+.home-nav {
   background-color: pink;
   color: white;
   font-weight: 600;
